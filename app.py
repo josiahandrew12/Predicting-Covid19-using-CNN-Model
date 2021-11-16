60c7dc2a5ec3fd7c2179bb4aadfd7f6993aeb0c5
Jiyoung Yoon <ebbuni1023@users.noreply.github.com>
2021-11-16 01:57:07 -0400

    basic

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-
 def hello():
-    return "Hello!"
+    return render_template("index.html")
 
+@app.route('/upload')
+def upload():
+    return render_template("upload.html")
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
