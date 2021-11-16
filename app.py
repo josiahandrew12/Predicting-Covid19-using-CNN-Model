ef47eab00381aa6cd57d9e989818d414cfe20905
Jiyoung Yoon <ebbuni1023@users.noreply.github.com>
2021-11-16 02:07:11 -0400

    update

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return render_template("index.html")
 
-@app.route('/upload')
+@app.route('/upload', methods = ['POST'])
 def upload():
-    return render_template("upload.html")
+    # HTML -> .py
+    if request.method == 'POST':
+        name = request.form("file")
+    
+    # .py -> HTML
+    return render_template("upload.html", chestImage = name)
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
