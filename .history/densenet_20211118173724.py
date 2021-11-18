13066d6b704235ad7bf825c0b40ba7e9e053d0bc
josiahcornelius <josiahandrew11@gmail.com>
2021-11-18 17:44:52 -0500

    turned model.py into 3 files

---
+++ .history/densenet_20211118173724.py
@@ -0,0 +1,17 @@
+def build_densenet():
+    densetnet201 = DenseNet201(weights='imagenet', include_top=False)
+    input = Input(shape=(224, 224, 3))
+    x = Conv2D(3, (3, 3), padding='same')(input)
+    x = densetnet201(x)
+    x = GlobalAveragePooling2D()(x)
+    x = BatchNormalization()(x)
+    x = Dense(128, activation='relu')(x)
+    x = BatchNormalization()(x)
+    x = Dropout(0.3)(x)
+    x = Dense(64, activation='relu')(x)
+    x = BatchNormalization()(x)
+    output = Dense(2,activation = 'softmax', name='root')(x)
+    model = Model(input,output)
+    optimizer = Adam(lr=0.003, beta_1=0.9, beta_2=0.999, epsilon=0.1, decay=0.0)
+    model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])    
+    return model
