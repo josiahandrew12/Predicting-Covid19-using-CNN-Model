8a5bcef42fb5823135dee9aab001f0f313cdade9
Joe <joe@MacBook-Pro-5.local>
2021-11-21 00:02:56 -0500

    update

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-
+#
 from flask import Flask, render_template, request
-from scripy.msc import imsave, imread, imresize
+from scipy.misc import imsave, imread, imresize
 import numpy as np
 import keras.models
 import re
@@ -8,10 +8,14 @@ import sys
 import os 
 import base64
 from load import*
+global graph, model
+
+model, graph = init()
+app = Flask(__name__)
+
 
 sys.path.append(os.path.abspath())
 from werkzeug.wrappers import response
-app = Flask(__name__)
 
 @app.route('/')
 def homepage_view():
@@ -20,9 +24,17 @@ def homepage_view():
 # Predict route for our saved model
 @app.route('/predict', methods = ['GET', 'POST'])
 def predict():
-    response = "ML Prediction"
+    imageData = request.get_data()
+    convertImage(imageData)
+    x = imread('output.png', model='L')
+    x = np.invert(x)
+    x = imresize(x,(224,224))
+    x = x.reshape(1,224,224,1)
+
     return response
+
     
 if __name__ == '__main__':
     app.run(debug=True)
+
 
